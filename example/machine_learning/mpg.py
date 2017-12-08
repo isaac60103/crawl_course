52aa30eef3b8423ba28f7f1e9ac6268cc32969d9
isaac60103 <isaac60103@gmail.com>
2017-12-08 23:31:35 +0800

    add normalization to machine learning example

--- example/machine_learning/mpg.py
+++ example/machine_learning/mpg.py
@@ -1,50 +1,52 @@
-# coding: utf-8
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
-
-import math
-import numpy as np
+#讀取資料
 import pandas as pd
 
-df = pd.read_csv('auto-mpg.csv')
+df = pd.read_csv('mpg.csv')
 
+print(df.head())
+
+#去除多餘資料
 df = df.drop('name', axis=1)
 
+#格式化資料
 df['origin'] = df['origin'].replace({1: 'america', 2: 'europe', 3: 'asia'})
 
 df = pd.get_dummies(df, columns=['origin'])
 
+df.head()
+
+import numpy as np
+#去除missing data
 df = df.replace('?', np.nan)
 df = df.dropna()
 
+#標準化 :為了避免偏向某個變數去做訓練
+df = (df - df.mean()) / df.std()  
+
+#分割資料
 X = df.drop('mpg', axis=1)
 y = df[['mpg']]
+
+from sklearn.model_selection import train_test_split
 
 # Split X and y into X_
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=1)
 
+#代入模型training
+from sklearn.linear_model import LinearRegression
+
 regression_model = LinearRegression()
 regression_model.fit(X_train, y_train)
 
+#印出各系數
 for idx, col_name in enumerate(X_train.columns):
     print("The coefficient for {} is {}".format(col_name, regression_model.coef_[0][idx]))
 
+#截距
 intercept = regression_model.intercept_[0]
 
 print("The intercept for our model is {}".format(intercept))
 
+#將測試資料代入得準確率
 regression_model.score(X_test, y_test)
-
-y_predict = regression_model.predict(X_test)
-
-regression_model_mse = mean_squared_error(y_predict, y_test)
-
-regression_model_mse
-
-math.sqrt(regression_model_mse)
-
-result = regression_model.predict([[4, 121, 110, 2800, 15.4, 81, 0, 1, 0]])
-
-print(result)
 
--- practice/house_price_predict.py
+++ practice/house_price_predict.py
@@ -1,36 +1,38 @@
-# coding: utf-8
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
-
-import math
+#讀取資料
 import pandas as pd
 
 df = pd.read_csv('house.csv')
 
+print(df.head())
+
+#標準化 :為了避免偏向某個變數去做訓練
+df = (df - df.mean()) / df.std()  
+
+#分割資料
 X = df.drop('price', axis=1)
 y = df[['price']]
+
+from sklearn.model_selection import train_test_split
 
 # Split X and y into X_
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=1)
 
+
+#代入模型training
+from sklearn.linear_model import LinearRegression
+
 regression_model = LinearRegression()
 regression_model.fit(X_train, y_train)
 
+#印出各系數
 for idx, col_name in enumerate(X_train.columns):
     print("The coefficient for {} is {}".format(col_name, regression_model.coef_[0][idx]))
 
+#截距
 intercept = regression_model.intercept_[0]
+
 print("The intercept for our model is {}".format(intercept))
 
+#將測試資料代入得準確率
 regression_model.score(X_test, y_test)
 
-y_predict = regression_model.predict(X_test)
-
-regression_model_mse = mean_squared_error(y_predict, y_test)
-
-math.sqrt(regression_model_mse)
-
-result = regression_model.predict([[1203,3]])
-
-print(result)
