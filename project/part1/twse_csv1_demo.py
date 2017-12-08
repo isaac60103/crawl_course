4f56d8b89eda2c05125ca157e4a44f06a7a3de37
isaac60103 <isaac60103@gmail.com>
2017-12-09 00:18:26 +0800

    fix some bug

--- project/part1/twse_csv1_demo.py
+++ project/part1/twse_csv1_demo.py
@@ -1,9 +1,6 @@
-# coding: utf-8
 import requests
 import json
 import pandas as pd
-import pandas.io.sql as pd_sql
-import sqlite3 as sql
 from time import sleep
 
 err_count = 0
@@ -13,6 +10,8 @@ while err_count < 3:
         res1 = requests.get('http://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20170901&stockNo=2330')
         sleep(5)
         res2 = requests.get('http://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20171001&stockNo=2330')
+        sleep(5)
+        res3 = requests.get('http://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20171101&stockNo=2330')
         break
     except:
         sleep(5)
@@ -23,15 +22,18 @@ if err_count == 3:
 
 json1 = res1.json()
 json2 = res2.json()
+json3 = res3.json()
 
 header = json1['fields']
 
 data = json1['data']
 data.extend(json2['data'])
+data.extend(json3['data'])
+
 
 df = pd.DataFrame.from_records(data, columns=header)
+df.sort_index(ascending=False).head()
 
-#日期轉換西元年
 for i, row in df.iterrows():
     #print(row[0])
     strDate = row[0]
@@ -41,16 +43,17 @@ for i, row in df.iterrows():
     DateArr[2] = str(int(DateArr[2]))
     #df.set_value(i,'日期','/'.join(DateArr))
     df.loc[i, '日期'] = '/'.join(DateArr)
+df.sort_index(ascending=False).head()
 
-#存成CSV
 df.to_csv('../data/fp_demo.csv', sep=',', encoding='utf-8-sig', index=False)
 
-#存入SqLite
+
+
+import sqlite3 as sql
+
 #連線至sqlite檔案，若無該檔案sql，則會建立一個新的
-#conn = sql.connect("../data/twse.db")
+conn = sql.connect("../data/twse.db")
 #將Dataframe資料寫入sql檔中的'demo1'表中，無該資料表則會自動建立
-#df.to_sql("demo1", conn, if_exists="replace")
+df.to_sql("demo1", conn, if_exists="replace", index=False)
 
-
-print('Done!')
-
+pd.read_sql_query("select * from demo1;", conn).sort_index(ascending=False)
